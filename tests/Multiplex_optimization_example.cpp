3a96cd0b249a9c4f5db66e1d282b477a8587070e
Henry Hollis <henryhollis4@gmail.com>
2024-02-02 10:11:28 -0500

    Finished Multiplex_optimization_example.cpp example

--- tests/Multiplex_optimization_example.cpp
+++ tests/Multiplex_optimization_example.cpp
@@ -11,6 +11,24 @@ using std::cout;
 using std::endl;
 
 int main(void) {
+    // Set a fixed seed for reproducibility
+    std::random_device rd;
+    std::mt19937 gen(42);  // Use std::mt19937 with a random seed from std::random_device
+
+    // Define the dimensions of the matrix
+    const size_t rows = 12;
+    const size_t cols = 7;
+
+    // Initialize the matrix with zeros
+    std::vector<std::vector<double>> geneSampleMatrix(rows, std::vector<double>(cols, 0.0));
+
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            geneSampleMatrix[i][j] = std::generate_canonical<double, 10>(gen);// Example: random doubles between 0 and 1
+        }
+    }
+
+
     igraph_t g;
     igraph_vector_int_t edges;
 
@@ -30,13 +48,16 @@ int main(void) {
     Graph graph(&g);
     ModularityVertexPartition part(&graph);
     ccdModularityVertexPartition part2(&graph);
-
+    part2.setGeneSampleMatrix(geneSampleMatrix);
+    std::vector<bool> isMembershipFixed(7, false);
     Optimiser o; //create optimiser o
-
+    o.optimise_partition({&part, &part2}, {1., 1.},isMembershipFixed);
     cout << "Node\tCommunity" << endl;
     for (int i = 0; i < graph.vcount(); i++)
         cout << i << "\t" << part.membership(i) << endl;
-
+    cout << "Node\tCommunity" << endl;
+    for (int i = 0; i < graph.vcount(); i++)
+        cout << i << "\t" << part2.membership(i) << endl;
 
     igraph_destroy(&g);
 
